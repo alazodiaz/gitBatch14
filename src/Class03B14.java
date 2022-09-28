84940c4bdea31f32e83383e4a9f2bf925dbad402
alazodiaz <andrealazo12@gmail.com>
2022-09-27 21:49:42 -0400

    GitClass4

---
+++ src/Class03B14.java
@@ -0,0 +1,14 @@
+
+public class Class03B14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hey this is a git class");
+		System.out.println("this is a change in my project");
+		System.out.println("How are u this is a new version");
+	
+
+	}
+
+}
